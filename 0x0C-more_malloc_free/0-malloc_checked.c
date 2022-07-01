9a0b9e5994bdb1d7fc5f9c9c8de01599ecbd85eb
wamtitu <wamtitujose@gmail.com>
2022-06-30 23:28:51 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	void  *a;
 
-	if (mem == NULL)
+	a = malloc(b);
+	if (a == NULL)
 		exit(98);
 
-	return (mem);
+	return (a)
 }
